aca3aceddfe91bbdba651a697a3898bfd92626cf
tfs-apps-one <furu12080205@gmail.com>
2024-10-13 16:00:43 +0900

    Initial Commit

---
+++ NoticeTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NoticeTimer
+//
+//  Created by 古川貴史 on 2024/10/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NoticeTimer/NoticeTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoticeTimerApp.swift
+//  NoticeTimer
+//
+//  Created by 古川貴史 on 2024/10/13.
+//
+
+import SwiftUI
+
+@main
+struct NoticeTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
